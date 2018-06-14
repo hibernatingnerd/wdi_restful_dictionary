59ae1ec0049a2c5aaecf4ceee0bf2a36b36da0df
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-14 17:13:54 -0400

    added CRUD functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,50 @@
 class EntriesController < ApplicationController
 
   def index
+    @entries = Entry.all
+  end
 
+  def new
+    @entry = Entry.new
   end
 
   def edit
-    
+    @entry = Entry.find(params[:id])
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
   end
 
   def update
-     redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+  if @entry.save
+    redirect_to entry_url(params[:id])
+  else
+    redirect_to new_entry_url
   end
+end
 
   def create
-      redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    @entry.save
+    redirect_to entries_path
+
+  end
+
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 
 end
